3fa7d3427acfbcfdcfac00e41a0292b4bf4e8636
sohan_99 <sohan75632@gmail.com>
2025-01-22 13:01:10 +0600

    initiali Setup complete

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Left from './home/left/Left'
+import Right from './home/right/Right'
+
+function App() {
+  return (
+    <>
+    App
+    <Left />
+    <Right />
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/home/left/Left.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Left() {
+  return (
+    <div>Left</div>
+  )
+}
+
+export default Left
